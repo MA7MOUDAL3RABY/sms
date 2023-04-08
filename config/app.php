2060934551f417ac2a46c86733579d1df5d89ae0
Araby <ma7moudal3raby@gmail.com>
2023-04-08 19:37:12 +0200

    init auth

--- config/app.php
+++ config/app.php
@@ -53,6 +53,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
+    'admin_url' => env('ADMIN_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
@@ -174,6 +175,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Mckenziearts\Notify\LaravelNotifyServiceProvider::class
 
     ],
 
